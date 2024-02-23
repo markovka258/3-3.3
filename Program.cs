38a30e89d8d447d62002bc6b53db941b3ec19446
Kornaukhova Maria <kornaukhova.masha@lit1533.ru>
2024-02-23 23:16:42 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,39 +1,48 @@
 ﻿﻿using System;
 internal class Program
+{
+    static void Main()
     {
+        Array1 one = new Array1();
+        Array2 two = new Array2();
+        Array3 three = new Array3();
+        DaysOfWeekPrinter daysOfWeekPrinter = new DaysOfWeekPrinter();
 
-        static void Main()
+        IPrinter[] printers = new IPrinter[]
         {
-            Array1 one = new Array1();
-            Array2 two = new Array2();
-            Array3 three = new Array3();
-            DaysOfWeekPrinter daysOfWeekPrinter = new DaysOfWeekPrinter();
+            one, two, three, daysOfWeekPrinter
+        };
 
-            IPrinter[] printers = new IPrinter[]
+        foreach (IPrinter printer in printers)
+        {
+            Console.WriteLine($"Элементы массива {printer.GetType()}:");
+            printer.Print();
+
+            if (printer is IArray array)
             {
-                one, two, three, daysOfWeekPrinter
-            };
-
-            foreach (IPrinter printer in printers)
-            {
-                Console.WriteLine($"Элементы массива {printer.GetType()}");
-                printer.Print();
-
-                if (printer is IArray array)
+                Console.WriteLine($"Среднее значение {array.GetType()} массива {array.GetAverage()}");
+                
+                if (array is Array1 array1)
                 {
-                    Console.WriteLine($"Среднее значение {array.GetType()} массива {array.GetAverage()}");
+                    Console.WriteLine($"Массив {array1.GetType()} с удаленными дубликатами");
+                    array.Print();
+
                 }
-            }
-            
-            Console.WriteLine($"Массив {one.GetType()} с удаленными дубликатами");
-            one.Print();
 
-            Console.WriteLine("Массив 2 змейкой:");
-            two.InReverse();
+                if (array is Array2 array2)
+                {
+                    Console.WriteLine("Массив 2 змейкой:");
+                    array2.InReverse();
+                }
 
-            foreach (double i in three.GetAverages())
-            {
-                Console.WriteLine($"Среднее для вложенного в array3 массива: {i}");
+                if (array is Array3 array3)
+                {              
+                    foreach (double i in three.GetAverages())
+                    {
+                        Console.WriteLine($"Среднее для вложенного в array3 массива: {i}");
+                    }
+                }
             }
         }
     }
+}
--- obj/Debug/net8.0/3-3-3.AssemblyInfo.cs
+++ obj/Debug/net8.0/3-3-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-3-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd2993dbc91bde888bb9c634b3e1a867ab41447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c5866dfd9f5cab4691f5b63988f956c6e16fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-3-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-3-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
